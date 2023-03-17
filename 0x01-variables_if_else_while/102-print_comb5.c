805b6c042a30b749d9a044b23e22dcace2487df2
Harry Muriithi <hharryjjack@gmail.com>
2023-03-17 10:09:46 -0700

    task 13 2  program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point for the program
  *Return: Always 0 (Success)
@@ -12,21 +11,21 @@ for (i = 0; i < 100; i++)
 {
 for (j = i; j < 100; j++)
 {
-if (i != j)
-{
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
+int num1 = i / 10;
+int num2 = i % 10;
+int num3 = j / 10;
+int num4 = j % 10;
+putchar(num1 + '0');
+putchar(num2 + '0');
 putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-if (i != 99 || j != 98)
+putchar(num3 + '0');
+putchar(num4 + '0');
+if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-}
-putchar('\n');
 return (0);
 }

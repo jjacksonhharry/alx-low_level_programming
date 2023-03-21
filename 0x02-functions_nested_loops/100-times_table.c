72a3b65c47c4ab7b309f9ec9c43ebc65d662d0cc
Harry Muriithi <hharryjjack@gmail.com>
2023-03-21 15:20:42 -0700

    task 13 a program that computes and prints the sum of all the multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,20 +6,22 @@
  */
 
 void print_times_table(int n)
-
 {
 	int i, j, res;
 
 	if (n < 0 || n > 15)
-		return;
+	return;
 
 	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			res = i * j;
-			printf("%d\t", res);
-		}
-		printf("\n");
-	}
+{
+	for (j = 0; j <= n; j++)
+{
+	res = i * j;
+	if (j == 0)
+	printf("%d", res);
+	else
+	printf(", %d", res);
+}
+	printf("\n");
+}
 }
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+int limit = 1024;
+int sum = 0;
+for (int i = 1; i < limit; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}

b1e16c4820c039923e7707772b0e69ce102a3b54
Harry Muriithi <hharryjjack@gmail.com>
2023-03-21 14:54:52 -0700

    task 12 a function that prints the n times table, starting with 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_times_table - prints multiplication table
+ * @n: integer
+ * Return: 0 on success
+ */
+
+void print_times_table(int n)
+
+{
+	int i, j, res;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+			printf("%d\t", res);
+		}
+		printf("\n");
+	}
+}

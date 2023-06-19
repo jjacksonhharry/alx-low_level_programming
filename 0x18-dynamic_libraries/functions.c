addb3a8717a4e3e4c34cf65c0f2c934c9b89a2fb
Harry Muriithi <hharryjjack@gmail.com>
2023-06-19 11:09:21 -0700

    dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - Function that adds two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: The Sum of parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Function that subtracts two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: The Subtraction of parameters
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Function that multiplies two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: The Multiplication of parameters
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Function that divides two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: The Division of parameters
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Function that find the mod for two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: The Mod of parameters
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
